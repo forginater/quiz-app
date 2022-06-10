179789ecfb677a5b695adb86ee0f2e062e44bd5a
forginater <alex.w.forge@gmail.com>
2022-06-10 23:09:37 +1000

    Implemented genRandNum(min,max) that generates a random integer in the inclusive range between min-max

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,13 @@ import './App.css';
 Randomly generate 2 integers within a certain range
 */
 
-//genRandNum() generates a random integer in the inclusive range between 'min' and 'max'
-function genRandNum(min: number, max: number): number {
-  //delta = magnitude of the range from min to max
-  const delta = min-max;
-  //Generate a random floating-point num in range 0 (inclusive) to 1 (excluding)
-  const randNum = Math.random();
-  //Scale randNum to fall within delta range
-  const randScaled = randNum * delta;
-  //translate randScaled so it's in the range between min and max
-  const randTranslated = randScaled + min;
-  //round down to integer
-  const floored = Math.floor(randTranslated);
-  return floored;
-}
 
-export function genRandInclusiveRange(min: number, max: number) {
-  //delta has same magnitude as range but relative to 1
-  const delta = max-min; //Maybe need to add 1
-    //The Math.random() function returns a floating-point, random          
-    //number in the range from 0 (inclusive) up to but not including 1.
-    const initialRand = Math.random();
-    //Scale random number so it falls within the delta range (relative to 1)
-    const randScaled = initialRand * delta;
-    //translating by min shifted randShifted so it's in the correct range between min (inclusive) & max (exclusive)
-    const randShifted = randScaled + min;
-    //floored is rounded down to next integer (so it will be inclusive of both min & max values)
-    const floored = Math.floor(randShifted);
-    return floored;
-}
+
+
 
 
 function App() {
+  testRandFunc();
   return (
     <div className="App">
       <header className="App-header">
@@ -102,5 +77,27 @@ function DisplayResult(props: {correct: boolean}) {
   )
 }
 
-export default App;
 
+//genRandNum() generates a random integer in the inclusive range between 'min' and 'max'
+function genRandNum(min: number, max: number): number {
+  //delta = magnitude of the range from min to max 
+  const delta = max-min;
+  //Generate a random floating-point num in range 0 (inclusive) to 1 (excluding)
+  const randNum = Math.random();
+  //Scale randNum to fall within delta range
+  const randScaled = randNum * (delta + 1);
+  //translate randScaled so it's in the range between min and max
+  const randTranslated = randScaled + min;
+  //round down to integer
+  const floored = Math.floor(randTranslated);
+  return floored;
+}
+
+function testRandFunc() {
+ for (let i=0; i<30; i++) {
+  console.log(i,": ",genRandNum(0,10));
+ }
+}
+
+
+export default App;
