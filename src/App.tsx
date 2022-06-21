81c5ed18495481f6f19902d703818a58dbef9958
forginater <alex.w.forge@gmail.com>
2022-06-21 21:50:11 +1000

    Last commit before modify App & EnterSettings to remove doneSettings

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,19 @@ import { EnterSettings } from './EnterSettings';
 
 
 
-
 //Get settings from user then render the quiz, finally display results to user
 function App() {
   //timeLimit is hardcoded as 10 by default but can be changed by user in the <EnterSettings> component
   const [timeLimit, setTimeLimit] = useState<number>(10);
+  /*
+  const [numQuestions, setNumQuestions] = useState<number>(10);
+  const [upperBound, setUpperBound] = useState<number>(1);
+  const [lowerBound, setLowerBound] = useState<number>(10);
+  */
   //paramsEntered
   const [doneSettings, setDoneSettings] = useState(false);
+
+
 
 
   return (
@@ -35,3 +41,23 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+/*
+interface Settings {
+  timeLimit: number;
+  numQuestions: number;
+  upperBound: number;
+  lowerBound: number;
+}
+
+const initEntryProps: EnterSettingsProps = {
+  timeLimit: timeLimit,
+  numQuestions: numQuestions,
+  upperBound: upperBound,
+  lowerBound: lowerBound
+}
+*/
--- src/EnterSettings.tsx
+++ src/EnterSettings.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './App.css';
 import {useState, useEffect} from 'react';
 
+/*
+export interface EnterSettingsProps {
+  timeLimit: number;
+  numQuestions: number;
+  upperBound: number;
+  lowerBound: number; 
+  updateSettings: any;
+}
+*/
 //Quiz settings must be custom entered or the default settings accepted before quiz is started
 export function EnterSettings(props: {timeLimit: number, setTimeLimit: (n: number) => void, setDoneSettings: (b: boolean) => void}) {
-    return (
+  return (
       <div>
         <h3>Enter Quiz Settings:</h3>
         <label>
@@ -18,6 +27,66 @@ export function EnterSettings(props: {timeLimit: number, setTimeLimit: (n: numbe
             }}
           />
         </label>
+        <br/><br/>
+        <label>
+          Enter number of questions
+          <input 
+            type="number"
+            //value={props.numQuestions ?? ''}
+            onChange={(e) => {
+              
+            }}
+          />
+        </label>        
+
+        <br/><br/>
+        <label>
+          <input
+            type="button"
+            value="Start Quiz"
+            onClick={(e) => {
+              isNaN(props.timeLimit)
+                ? console.log("Please enter an integer timeLimit > 0!") 
+                : props.setDoneSettings(true);
+
+            }}
+          />
+            
+        </label>
+  
+      </div>
+    )
+  }
+
+
+//Quiz settings must be custom entered or the default settings accepted before quiz is started
+export function EnterSettingsOld(props: {timeLimit: number, setTimeLimit: (n: number) => void, setDoneSettings: (b: boolean) => void}) {
+  return (
+      <div>
+        <h3>Enter Quiz Settings:</h3>
+        <label>
+          Enter time limit in seconds:
+          <input 
+            type="number"
+            value={props.timeLimit ?? ''}
+            onChange={(e) => {
+              props.setTimeLimit(parseInt(e.target.value)); 
+              
+            }}
+          />
+        </label>
+        <br/><br/>
+        <label>
+          Enter number of questions
+          <input 
+            type="number"
+            //value={props.numQuestions ?? ''}
+            onChange={(e) => {
+              
+            }}
+          />
+        </label>        
+
         <br/><br/>
         <label>
           <input
