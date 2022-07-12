9edcd60ed782245f60af9316026b2b96fdd8a2da
forginater <alex.w.forge@gmail.com>
2022-07-12 16:17:29 +1000

    moved DisplayHumiliation() & PatheticHumanWeep() function components out of BattleField

--- src/BattleField.tsx
+++ src/BattleField.tsx
@@ -8,6 +8,7 @@ import {genRandNum} from './genRandNum';
 import {QuizQuestion, QuizQuestionProps, ViewState, ViewStateNewLine} from './QuizQuestion';
 import { count } from 'console';
 import { BasicCounter } from './TimerComponents';
+import { DisplayHumiliation } from './DisplayHumiliation';
 
 
 /* 
@@ -316,44 +317,3 @@ function buildOutcomes(numQuestions: number): Outcome[] {
 
 
 
-
-type ZippedTuple = [question: Question, outcome : Outcome];
-interface PatheticHumanWeepProps {zip: ZippedTuple;}
-function DisplayHumiliation(props: {outcomes: Outcome[], questions: Question[]}) {
-    //Combine each question/outcome pair a tuple 
-    const zippedArr: ZippedTuple[] = props.questions.map((question,indexZip) => {
-        return [question,props.outcomes[indexZip]];
-    });
-    return (
-      <>
-        <h1>Quiz Completed!!</h1>
-        <li>
-                {zippedArr.map((res) => <PatheticHumanWeep zip={res} />)}
-        </li>
-      </>
-    )
-  }
-
-
-function PatheticHumanWeep(props: PatheticHumanWeepProps) {
-    const [question,outcome] = props.zip;
-    const questStr = `Question: ${question.num1} x ${question.num2} = ${question.num1*question.num2}`;
-    
-    //Return result of outcome unless undefined
-    const validOutcome = outcome.result === 'TimeUp' || outcome.result === 'Incorrect' || outcome.result || 'Correct';
-    let resultStr = validOutcome 
-        ? `Outcome: ${outcome.result}` 
-        : `Something Went Wrong`;
-
-    return ( 
-        <dl>
-            <dd>
-                {questStr} 
-            </dd>
-            <dd>
-                {resultStr}
-            </dd>
-        </dl>
-
-    )
-}
